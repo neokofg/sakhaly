7917513ecd933dace1a247e851947dd043e3e8c4
neoko <33611354+almasz243@users.noreply.github.com>
2023-02-16 22:58:26 +0300

    Update UserApiController.php

--- app/Http/Controllers/UserApiController.php
+++ app/Http/Controllers/UserApiController.php
@@ -53,8 +53,7 @@ class UserApiController extends Controller
         $user_id = $json['user_id'];
         $type = $json['type'];
         $amount = $json['amount'];
-        $realTime = Carbon::today();
-        print($realTime);
+        $realTime = date("Y-m-d");
         $user = User::where('id',$user_id)->get();
         foreach($user as $userItem){
             $decodedStats = json_decode($userItem->stats,true);
