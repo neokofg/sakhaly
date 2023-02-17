dbb269b5b3d5ac311b660178f01b57535a9b69c0
neoko <33611354+almasz243@users.noreply.github.com>
2023-02-17 04:50:39 +0300

    Update RoomApiController.php

--- app/Http/Controllers/RoomApiController.php
+++ app/Http/Controllers/RoomApiController.php
@@ -251,6 +251,7 @@ class RoomApiController extends Controller
                     'error' => 'Answer"s type is not valid'
                 ], 401);
             }
+            print($answers[$key]);
             if($answers[$key] !== $answer){
                 $decodedUsers['users'][$user_id]['balls'] = $decodedUsers['users'][$user_id]['balls'] + 5;
             }
